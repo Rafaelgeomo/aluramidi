da0af15b5d5cb6d4de74b5df3810aba6fd6bd009
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-01-30 18:52:47 -0300

    Inicio Alura Midi

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+ 
+} 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador++;
+
+}
+
+
