94ba75a87ca3f02b7fa5cdb6decd087517ed65c6
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-02-09 16:05:22 -0300

    inclusão para mexer com as teclas do teclado

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
  
 } 
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+for (let contador=0; contador < listaDeTeclas.length; contador++) {
 
-    contador++;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];     
+    const idAudio = `#som_${instrumento}`; //template string
+    
+     tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) { //evento quando aperta uma tecla do teclado
+              
+        if (evento.code === 'Space'||evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa'); //evento quando soltar uma tecla do teclado
+    }
 
 }
 
