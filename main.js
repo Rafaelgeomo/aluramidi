9851ac2b185824b967284f9630d8ab8f1caca360
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2023-02-14 17:10:31 -0300

    Conclusão

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+   if (elemento && elemento.localName === 'audio') { //Se elemento for nulo E nome do elemento for audio tocar, senao dar a msg abaixo
+        elemento.play();
+   }
+   else {
+     console.log('Elemento não encontrado ou seletor inválido');
+   }
  
 } 
 const listaDeTeclas = document.querySelectorAll('.tecla');
